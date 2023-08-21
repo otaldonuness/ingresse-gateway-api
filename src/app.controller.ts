ae4c1ead0d712479e875618546a8f09b5317c034
Arthur Henrique Teixeira Nunes <otaldonunes@MacBook-Air-de-Arthur.local>
2023-08-21 13:59:03 -0300

    feat: prometheus e grafana

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TerminusModule,
+} from '@nestjs/terminus';
 
 @Controller()
 export class AppController {
@@ -8,5 +13,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+
+@Controller('api/health')
+export class HealthController {
+  constructor(private health: HealthCheckService) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    // Add checks here
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { CustomThrottlerModule } from './throttler/throttler.module';
 import { WinstonModule } from './logger/winston.module';
 import { MongooseModule } from './database/mongoose.module';
 import { ConfigModule } from './config/config.module';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [ConfigModule, CustomThrottlerModule, WinstonModule, MongooseModule],
+  imports: [
+    ConfigModule,
+    CustomThrottlerModule,
+    WinstonModule,
+    MongooseModule,
+    PrometheusModule.register({
+      defaultLabels: {
+        app: 'ingresse-gateway-api',
+        path: '/api/metrics',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
